d1d80c664c7262fdfe93b3deaa2592555b22e3fa
jasonxue <jason@xuechunxi.com>
2025-11-15 19:54:28 +0800

    clash-verge-rev: 2.4.2 -> 2.4.3

--- pkgs/clash-verge-rev/default.nix
+++ pkgs/clash-verge-rev/default.nix
@@ -5,7 +5,7 @@
   undmg,
 }: let
   pname = "clash-verge-rev";
-  version = "2.4.2";
+  version = "2.4.3";
 
   metaBase = {
     description = "Clash GUI based on tauri";
@@ -17,13 +17,13 @@
     aarch64-darwin = {
       src = fetchurl {
         url = "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v${version}/Clash.Verge_${version}_aarch64.dmg";
-        sha256 = "sha256-V7COnyVLsaNcHHr8iK9X2a3ZapUrFavQx9ycYrEojmY=";
+        sha256 = "sha256-V78OnyVLsaNcHHr8iK9X2a3ZapUrFavQx9ycYrEojmY=";
       };
     };
     x86_64-darwin = {
       src = fetchurl {
         url = "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v${version}/Clash.Verge_${version}_x64.dmg";
-        sha256 = "sha256-Gk6l7yuja9/6wI1KODHyYP2VG7YoUEz9wVKNry/kPJE";
+        sha256 = "sha256-CvzfQm+yGXDwLFdWfU4BBByQpuMZ3hOmomhPOFBPtPE=";
       };
     };
   };
